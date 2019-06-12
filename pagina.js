3ebf012075367dbc88aea4ca32e59f7f1ec5018f
Cleiton Campos <Cleitoncampos2015@gmail.com>
2019-06-12 16:39:40 -0300

    adicionar alteracao

--- pagina.js
+++ pagina.js
@@ -1,66 +1,74 @@
-// // // // let idade = 105;
-// // // // idade +=1;
-// // // // console.log(idade); 
+// // // // // let idade = 105;
+// // // // // idade +=1;
+// // // // // console.log(idade); 
 
-// // // let nome = "Cleiton";
-// // // let cargo = "Programador";
-// // // let signo = "Aries";
+// // // // let nome = "Cleiton";
+// // // // let cargo = "Programador";
+// // // // let signo = "Aries";
 
-// // // alert(``)
+// // // // alert(``)
 
-// // let degrau = Number(prompt("Quantos degraus você quer para construir sua escada:"));
-// // let i = 0;
+// // // let degrau = Number(prompt("Quantos degraus você quer para construir sua escada:"));
+// // // let i = 0;
 
-// // for (i = degrau; i== degrau; i++){
+// // // for (i = degrau; i== degrau; i++){
 
 
-// //     console.log( `A quantidade de degraus é: ${degrau}`);
+// // //     console.log( `A quantidade de degraus é: ${degrau}`);
 
+// // // }
+
+// // let materialEscada = prompt('Digite o material da escada');
+// // let degrau = materialEscada;
+// // let qntd = Number(prompt("opa, quantos degraus?"));
+// // for(let i = 0; i<qntd; i++){
+// //     console.log(materialEscada);
+// //     materialEscada += degrau;
 // // }
 
-// let materialEscada = prompt('Digite o material da escada');
-// let degrau = materialEscada;
-// let qntd = Number(prompt("opa, quantos degraus?"));
-// for(let i = 0; i<qntd; i++){
-//     console.log(materialEscada);
-//     materialEscada += degrau;
+
+//     //     console.log("você pode assistir todos os filmes.");
+    
+//     // }else if (filmes[0].idade > 16 && filmes[0].idade < 18){
+    
+//     //     console.log("você só pode assistir o filme It a coisa");
+    
+//     // }else{
+//     //     console.log(filmes[1]);
+//     // }
+    
+    
+    
+    
+
+    
+    
+// // let objeto = {
+// //     casa:[],
+// //     carro:[]
+// // }
+
+// let selecionar = Number(prompt("Digite sua idade:"));
+// let filmes = [
+
+//     {titulo: "Harry Potter", idade:10, ator: "Daniel"},
+//     {titulo: "It a coisa", idade:16},
+//     {titulo:"O albergue", idade:18}
+// ]
+
+// for (valores of filmes){
+
+
+
+
+
+
 // }
 
-
-    //     console.log("você pode assistir todos os filmes.");
-    
-    // }else if (filmes[0].idade > 16 && filmes[0].idade < 18){
-    
-    //     console.log("você só pode assistir o filme It a coisa");
-    
-    // }else{
-    //     console.log(filmes[1]);
-    // }
-    
-    
-    
-    
+function soma(){
 
     
-    
-// let objeto = {
-//     casa:[],
-//     carro:[]
-// }
-
-let selecionar = Number(prompt("Digite sua idade:"));
-let filmes = [
-
-    {titulo: "Harry Potter", idade:10, ator: "Daniel"},
-    {titulo: "It a coisa", idade:16},
-    {titulo:"O albergue", idade:18}
-]
-
-for (valores of filmes){
-
-
-
-
-
+  
 
 }
+
